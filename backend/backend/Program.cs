480a769ba2a2addeccc7b578913a97f121c565e1
manasserom <manasseromm@gmail.com>
2023-06-09 09:56:24 +0200

    Cors policy added

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,10 +1,20 @@
 using backend.Models;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
+//add cors policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost/");
+                      });
+});
 
 
-// Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
